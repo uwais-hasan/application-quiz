fd9fb09a5bba0cc5309d9a80ea6be793de5ed99a
owies hassan <owies.h.hassan@gmail.com>
2023-07-09 12:42:35 +0300

    update name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import {Box} from "@mui/material";
 const Header = () => {
     return (
         <Box pt={2} className='flex'>
-            <h1 className='title animate__animated animate__rubberBand '>owies</h1>
+            <h1 className='title animate__animated animate__rubberBand '>Quiz Game</h1>
         </Box>
     )
 }
