d2a1cf73c90d27960fd5acdca4a62046a272cdea
uwais hasan <owies.h.hassan@gmail.com>
2024-03-03 16:03:04 +0200

    update some issue

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer=()=>{
     return(
         <Box pt={2}>
-            <Typography color={'#898989'} pt={2} className='border-footer'>COPTRIGHT/OWIES HASSAN</Typography>
+            <Typography color={'#898989'}  pt={2} className='border-footer'>COPTRIGHT/UWAIS HASAN</Typography>
         </Box>
     )
 }
